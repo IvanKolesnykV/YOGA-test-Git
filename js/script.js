51aef1066bc62da07cecab7846b7030455408091
Ivan <IvanKolesnykV@gmail.com>
2017-11-13 15:05:40 +0200

    fixet header when scrolling

--- js/script.js
+++ js/script.js
@@ -5,3 +5,27 @@ $('.nav-click').click(function() {
 	$('.span-opacity').toggleClass('open-span-span');
 	$('.span-rotate-botton').toggleClass('open-span-span-span');
 });
+
+//fixet header when scrolling
+var header = $('#header');
+var heightHeader = $('#header').outerHeight(true);
+
+window.onscroll = function(){
+if (window.pageYOffset==0){
+$('#header').css({
+        left: 0,
+        right: 0,
+        top: 0,
+        position: 'relative'
+    });
+$('#slider').css({"margin-top" : "0"});
+} else{
+$('#header').css({
+        left: 0,
+        right: 0,
+        top: 0,
+        position: 'fixed'
+    });
+$('#slider').css({"margin-top" : heightHeader + "px"});
+}
+};
